d3f3e979e26bcfe5bbfaadf9d85b18785c9235b9
Elena <malkovva@gmail.com>
2022-07-10 17:46:17 +0300

    edited Exercise 2

--- Exercise2/Program.cs
+++ Exercise2/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+Console.WriteLine("Введите целое число a");
+int numberA = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите целое число b");
+int numberB = int.Parse(Console.ReadLine());
+
+if (numberB > numberA)
+{
+    Console.WriteLine("max = " + numberB + " min = " + numberA);  
+}
+if (numberA > numberB)
+{
+    Console.WriteLine("max = " + numberA + " min = " + numberB);
+}
+if (numberB == numberA)
+{
+    Console.WriteLine("числа равны");  
+}
---
+++ Exercise4/Program.cs
@@ -0,0 +1,5 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+Console.WriteLine("Введите целое число a");
+int numberA = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите целое число b");
+int numberB = int.Parse(Console.ReadLine());
