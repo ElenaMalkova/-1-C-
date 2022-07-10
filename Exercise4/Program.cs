b35761f69b736660251a2cfc31b0ce05dec810e4
Elena <malkovva@gmail.com>
2022-07-10 18:00:24 +0300

    added Find Max of Three exercise

--- Exercise4/Program.cs
+++ Exercise4/Program.cs
@@ -3,3 +3,15 @@ Console.WriteLine("Введите целое число a");
 int numberA = int.Parse(Console.ReadLine());
 Console.WriteLine("Введите целое число b");
 int numberB = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите целое число c");
+int numberC = int.Parse(Console.ReadLine());
+int max = numberA;
+if (numberB > max)
+{
+   max = numberB; 
+}
+if (numberC > max)
+{
+    max = numberC;
+}
+Console.WriteLine("max = " + max);
